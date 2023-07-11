a220fceeb9fda3fcb96f4b6c9223d1a747f47c55
Tony Airian <tonyairian98@gmail.com>
2023-07-11 13:43:44 +0530

    second commit

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,12 +1,18 @@
-const mongoose=require('mongoose')
- mongoose
-  .connect("mongodb://localhost:27017/my_project", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://tonyairian98:ashique2@cluster0.r6aii2l.mongodb.net/dochere",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("DB connection Successfull");
   })
   .catch((err) => {
     console.log(err.message);
   });
+
+// ("mongodb://localhost:27017/my_project");
+// mongodb://localhost:27017
--- backend/index.js
+++ backend/index.js
@@ -70,10 +70,12 @@ try {
 
     socket.on("sendMessage", ({ senderId, receiverId, text }) => {
       const user = getUser(receiverId);
+
       io.to(user?.socketId).emit("getMessage", {
         senderId,
         text,
       });
+      
     });
 
     socket.on("disconnect", () => {
